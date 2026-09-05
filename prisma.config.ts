cfce3641435116e6170bc49117741107cfde8982
Johnny desktop <a125008@g.kogakuin.jp>
2026-09-05 11:14:01 +0900

    add supabase setting

--- prisma.config.ts
+++ prisma.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_URL"),
   },
 });
--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,17 +1,46 @@
 import { writeFile } from "fs/promises";
 import path from "path";
+import { supabase } from "@/lib/supabase";
 
 export async function saveImage(file: File): Promise<string | null> {
+    const useSupabaseStorage = process.env.NEXT_PUBLIC_USE_SUPABASE_STORAGE === "true";
+    if (useSupabaseStorage) {
+        return saveImageToSupabase(file);
+    } else {
+        return saveImageToLocal(file);
+    }
+}
+
+export async function saveImageToLocal(file: File): Promise<string | null> {
     const buffer = Buffer.from(await file.arrayBuffer())
     const fileName = `${Date.now()}_${file.name}`
     const uploadDir = path.join(process.cwd(), 'public/images')
 
     try {
         const filePath = path.join(uploadDir, fileName)
-    await writeFile(filePath, buffer)
+        await writeFile(filePath, buffer)
         return `/images/${fileName}`
-    } catch(error){
+    } catch (error) {
         return null
     }
 }
 
+async function saveImageToSupabase(file: File): Promise<string | null> {
+    const fileName = `${Date.now()}_${file.name}`;
+    const { data, error } = await supabase.storage
+        .from('blog_bucket')
+        .upload(fileName, file, {
+            cacheControl: '3600',
+            upsert: false,
+        });
+
+    if (error) {
+        console.error('Upload error:', error.message);
+        return null;
+    }
+    const {data: publicUrlData} = supabase.storage
+        .from('blog_bucket')
+        .getPublicUrl(fileName);
+
+    return publicUrlData.publicUrl;
+}
